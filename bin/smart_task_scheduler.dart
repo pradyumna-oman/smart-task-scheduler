b64f9ab6b60198a19271557fed0309d807ad7529
pradyumna-tec <pradyumnao.tech@gmail.com>
2026-07-04 21:29:24 +0530

    Add download progress animation

--- bin/smart_task_scheduler.dart
+++ bin/smart_task_scheduler.dart
@@ -1,55 +1,59 @@
-import 'dart:io';
+  import 'dart:io';
 
-Future<void> download(String fileType, int seconds) async
-{
-  print('Downloading $fileType...');
-  print('Estimated time: $seconds seconds');
-  print('\n');
-  await Future.delayed(Duration(seconds: seconds));
-
-  print('$fileType Downloaded Successfully.');
-}
-
-void main() async
-{
-print('\nApplication started\n');
-bool isRunning = true;
-do
-{ 
-  print('''
-  ==============================
-      Smart Task Scheduler
-  ==============================
-
-  1. Download Image
-  2. Download Video
-  3. Download PDF
-  4. Exit
-
-  Enter your choice:''');
-
-  int choice = int.parse(stdin.readLineSync() ?? '0');
-
-  switch(choice)
+  Future<void> download(String fileType, int seconds) async
   {
-    case 1:
-      await download("Image",2);
-      break;
-    case 2:
-      await download("Video",8);
-      break;
-    case 3:
-      await download("PDF",4);
-      break;
-    case 4:
-      print("Thank you for using Smart Task Scheduler.");
-      isRunning = false;
-      break;
-
-    default:
-      print('Invalid input');
+    int milliSeconds = (seconds * 1000) ~/ 5;
+    print('Downloading $fileType...');
+    print('Estimated time: $seconds seconds');
+    for(int progress = 20; progress <= 100; progress += 20)
+    {
+      print('$progress%');
+      await Future.delayed(Duration(milliseconds: milliSeconds));
+    }
+    
+    print('$fileType Downloaded Successfully.');
   }
-}while(isRunning);
 
-print('Application finished');
-}
+  void main() async
+  {
+  print('\nApplication started\n');
+  bool isRunning = true;
+  do
+  { 
+    print('''
+    ==============================
+        Smart Task Scheduler
+    ==============================
+
+    1. Download Image
+    2. Download Video
+    3. Download PDF
+    4. Exit
+
+    Enter your choice:''');
+
+    int choice = int.parse(stdin.readLineSync() ?? '0');
+
+    switch(choice)
+    {
+      case 1:
+        await download("Image",2);
+        break;
+      case 2:
+        await download("Video",8);
+        break;
+      case 3:
+        await download("PDF",4);
+        break;
+      case 4:
+        print("Thank you for using Smart Task Scheduler.");
+        isRunning = false;
+        break;
+
+      default:
+        print('Invalid input');
+    }
+  }while(isRunning);
+
+  print('Application finished');
+  }
